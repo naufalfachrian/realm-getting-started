bc7a242f42d4f9fbf9175138e7ac7509d46617c6
Naufal Fachrian <naufal.fachrian@gmail.com>
2018-07-11 10:39:52 +0700

    update main activity, creating and inserting object,

--- app/src/main/java/bungamungil/id/realmgettingstarted/MainActivity.kt
+++ app/src/main/java/bungamungil/id/realmgettingstarted/MainActivity.kt
@@ -2,11 +2,32 @@ package bungamungil.id.realmgettingstarted
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import bungamungil.id.realmgettingstarted.model.Person
+import bungamungil.id.realmgettingstarted.model.PersonAdapter
+import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapter = PersonAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        Realm.getDefaultInstance().executeTransaction {
+            val person = Person().apply {
+                name = "Bunga Mungil"
+                age = 9
+            }
+            it.insert(person)
+        }
+
+        val persons = Realm.getDefaultInstance().where(Person::class.java).findAll()
+        adapter.items.addAll(persons)
+        adapter.notifyDataSetChanged()
     }
 }
